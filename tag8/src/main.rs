1536889f236d1a26c8e82cd207412f670691080e
Nina Hammer <nina.hammer@mni.thm.de>
2023-12-08 22:22:27 +0100

    tag 8

--- tag8/src/main.rs
+++ tag8/src/main.rs
@@ -1,16 +1,23 @@
-use std::{fs, collections::HashMap};
+use std::{collections::HashMap, fs};
 
+use ::num::integer::lcm;
 use scanf::sscanf;
 
 fn main() {
     let input = parse_input(read_input());
-    let starting_nodes =find_starting_nodes(&input.1);
+    let starting_nodes = find_starting_nodes(&input.1);
 
-    println!("{}", starting_nodes.iter().map(|s| walk_instructions(&input, s)).max().unwrap());
+    println!(
+        "{}",
+        starting_nodes
+            .iter()
+            .map(|s| walk_instructions(&input, s))
+            .fold(1, lcm)
+    );
 }
 
 fn read_input() -> String {
-    fs::read_to_string("input_test").unwrap()
+    fs::read_to_string("input").unwrap()
 }
 
 fn parse_input(input: String) -> (Vec<usize>, HashMap<String, Vec<String>>) {
@@ -19,38 +26,44 @@ fn parse_input(input: String) -> (Vec<usize>, HashMap<String, Vec<String>>) {
     let map = inp.next().unwrap();
     let mut ret = HashMap::new();
 
-    let instructions = lr.trim().chars().map(|c| match c {
-        'L' => 0,
-        'R' => 1,
-        _ => panic!("da stimmt was nicht")
-    }).collect::<Vec<usize>>();
+    let instructions = lr
+        .trim()
+        .chars()
+        .map(|c| match c {
+            'L' => 0,
+            'R' => 1,
+            _ => panic!("da stimmt was nicht"),
+        })
+        .collect::<Vec<usize>>();
 
-    map.lines().for_each(|l| { 
+    map.lines().for_each(|l| {
         let mut k: String = "Error".to_string();
         let mut left: String = "Error".to_string();
         let mut right: String = "Error".to_string();
-        
+
         sscanf!(l, "{} = ({}, {})", k, left, right).unwrap();
 
         ret.insert(k, vec![left, right]);
-    
     });
 
     (instructions, ret)
 }
 
-fn find_starting_nodes<'a>(map: &'a HashMap<String, Vec<String>>) -> Vec<&'a str> {
-    map.iter().filter(|(n, _)| n.chars().nth(2).unwrap() == 'A').map(|(n, _)| n.as_str()).collect()
-} 
+fn find_starting_nodes(map: &HashMap<String, Vec<String>>) -> Vec<&str> {
+    map.iter()
+        .filter(|(n, _)| n.chars().nth(2).unwrap() == 'A')
+        .map(|(n, _)| n.as_str())
+        .collect()
+}
 
 fn walk_instructions(input: &(Vec<usize>, HashMap<String, Vec<String>>), start: &str) -> usize {
     let mut counter = 0;
     let mut pos = (start, input.1.get(start).unwrap());
-    let end: String = vec![start.chars().take(2).collect::<String>().as_str(), "Z"].iter().copied().collect();
 
-    while pos.0 != end {
+    while pos.0.chars().nth(2).unwrap() != 'Z' {
         let lr = input.0[counter % input.0.len()];
         pos = (pos.1[lr].as_str(), input.1.get(&pos.1[lr]).unwrap());
+
         counter += 1;
     }
 
